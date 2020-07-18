20b71221e05d13b611fefa250d83891a80610e2a
Eugene White <eugenewhite1994@gmail.com>
2020-07-18 03:22:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,10 +37,9 @@ def adding
  shipping_manifest["muskets"] = 2
  shipping_manifest["gun powder"] = 4
 
-shipping_manifest
-end
+
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
