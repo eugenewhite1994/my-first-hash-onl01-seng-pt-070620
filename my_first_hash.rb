e9df7f56ec7d5ec5e28376c478b326c33b11d15c
Eugene White <eugenewhite1994@gmail.com>
2020-07-18 02:58:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,6 +8,9 @@ def my_hash
 end
 
 def shipping_manifest
+  the_manifest = {
+    
+  }
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # Wle have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
